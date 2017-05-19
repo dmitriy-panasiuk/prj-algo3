17914273dad0bc041e0fe49954146f903a89bc05
dmitriy-panasiuk <dmitriy.panasyuk@gmail.com>
2017-05-19 14:12:49 +0300

    Done for math expressions

--- tasks/dmitriypanasiuk/Regexp.java
+++ tasks/dmitriypanasiuk/Regexp.java
@@ -1,28 +1,52 @@
 package dmitriypanasiuk;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Regexp {
+    private static Map<Character, Integer> oPriorities = new HashMap<>();
+    static {
+        oPriorities.put('+', 1);
+        oPriorities.put('-', 1);
+        oPriorities.put('*', 2);
+        oPriorities.put('/', 2);
+    }
 
     public static String infixToPostfix(String expression) {
         StringBuilder postfix = new StringBuilder();
         Stack<Character> stack = new Stack<>();
 
         for (Character c : expression.toCharArray()) {
-            switch (c) {
-                case '(':
-                    stack.push(c);
-                    break;
-                default:
-                    postfix.append(c);
+            if (c == '(') stack.push(c);
+            else if (c == ')') {
+                while (stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop();
             }
+            else if (oPriorities.keySet().contains(c)) {
+                if (stack.isEmpty() || stack.peek() == '(' || oPriorities.get(c) > oPriorities.get(stack.peek())) {
+                    stack.push(c);
+                } else {
+                    while (!stack.isEmpty() && stack.peek() != '(' && oPriorities.get(stack.peek()) >= oPriorities.get(c)) {
+                        postfix.append(stack.pop());
+                    }
+                    stack.push(c);
+                }
+
+            }
+            else postfix.append(c);
+        }
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
         }
 
-        return expression;
+        return postfix.toString();
     }
 
     public static void main(String[] args) {
-        String expression = "(6+5*2)/4";
+        String expression = "(a*(b+c)+d)/2";
 
         System.out.println(infixToPostfix(expression));
     }
