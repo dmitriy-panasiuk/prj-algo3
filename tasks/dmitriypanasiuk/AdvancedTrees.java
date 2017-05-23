57da98719d628bee8454d89f05fe18b0a54d8f43
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2017-05-23 23:31:28 +0300

    Added code for intersection between two vectors

--- tasks/dmitriypanasiuk/AdvancedTrees.java
+++ tasks/dmitriypanasiuk/AdvancedTrees.java
@@ -63,6 +63,33 @@ class Polygon {
         return (intersections & 1) == 1;
     }
 
+    private boolean areIntersecting(double v1x1, double v1y1, double v1x2, double v1y2,
+                                    double v2x1, double v2y1, double v2x2, double v2y2) {
+        double d1, d2;
+        double a1, a2, b1, b2, c1, c2;
+        a1 = v1y2 - v1y1;
+        b1 = v1x1 - v1x2;
+        c1 = (v1x2 * v1y1) - (v1x1 * v1y2);
+
+        d1 = (a1 * v2x1) + (b1 * v2y1) + c1;
+        d2 = (a1 * v2x2) + (b1 * v2y2) + c1;
+
+        if (d1 > 0 && d2 > 0) return false;
+        if (d1 < 0 && d2 < 0) return false;
+
+        a2 = v2y2 - v2y1;
+        b2 = v2x1 - v2x2;
+        c2 = (v2x2 * v2y1) - (v2x1 * v2y2);
+
+        d1 = (a2 * v1x1) + (b2 * v1y1) + c2;
+        d2 = (a2 * v1x2) + (b2 * v1y2) + c2;
+
+        if (d1 > 0 && d2 > 0) return false;
+        if (d1 < 0 && d2 < 0) return false;
+
+        return true;
+    }
+
     Rectangle mbr() {
         if (this.mbr == null) {
             double xmin = Double.POSITIVE_INFINITY, xmax = Double.NEGATIVE_INFINITY, ymin = Double.POSITIVE_INFINITY, ymax = Double.NEGATIVE_INFINITY;
